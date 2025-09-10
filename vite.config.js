07d468c034d4be81e508f462fbf9bcc3ad653896
Cheyenne <cheyennejjohnson96@gmail.com>
2025-09-10 19:54:23 -0400

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite'
-<<<<<<< HEAD
 import react from '@vitejs/plugin-react'
-=======
-import react from '@vitejs/plugin-react-swc'
->>>>>>> e89c5f3158f0ce0ef97cae838ea948ecf739bc92
-
+import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),tailwindcss()], 
+
 })
