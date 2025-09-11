eff8e838972335e6f7e90d82858aeba4a7d8f39e
Dennis <dennis-farris@hotmail.com>
2025-09-11 11:17:55 -0400

    refactored authprovider to provide api url in a useref

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,31 +1,32 @@
 import axios from "axios";
-import { useMemo, useState } from "react";
+import { useMemo, useState, useRef } from "react";
 import { AuthContext } from "./AuthContext";
 
 export const AuthProvider = ({ children }) => {
   const [token, setToken] = useState(null);
   const [user, setUser] = useState(null);
 
-  const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
+  const apiRef = useRef(
+    axios.create({
+      baseURL: import.meta.env.VITE_API_URL,
+      headers: { "Content-Type": "application/json" },
+    })
+  );
 
-  // eslint-disable-next-line react-hooks/exhaustive-deps
   useMemo(() => {
-    api.interceptors.request.use((config) => {
+    apiRef.interceptors.request.use((config) => {
       if (token) {
         config.headers.Authorization = `Bearer ${token}`;
       }
 
       return config;
     });
-  });
+  }, [token]);
 
   return (
-    <AuthContext.Provider value={{ token, setToken, api, user, setUser }}>
+    <AuthContext.Provider
+      value={{ token, setToken, api: apiRef.current, user, setUser }}
+    >
       {children}
     </AuthContext.Provider>
   );
