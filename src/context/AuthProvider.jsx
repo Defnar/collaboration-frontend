379d7a7c1fc3bbfc9aefddf07e2a94426f406d56
Dennis <dennis-farris@hotmail.com>
2025-09-11 10:57:39 -0400

    set up auth to hold user details as well

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -4,11 +4,13 @@ import { AuthContext } from "./AuthContext";
 
 export const AuthProvider = ({ children }) => {
   const [token, setToken] = useState(null);
+  const [user, setUser] = useState(null);
 
   const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
   });
 
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   useMemo(() => {
     api.interceptors.request.use((config) => {
       if (token) {
@@ -20,7 +22,7 @@ export const AuthProvider = ({ children }) => {
   });
 
   return (
-    <AuthContext.Provider value={{ token, setToken, api }}>
+    <AuthContext.Provider value={{ token, setToken, api, user, setUser }}>
       {children}
     </AuthContext.Provider>
   );
