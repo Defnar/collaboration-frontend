2a50ee0c592266bdd362ed10ae31a71702d808b8
Dennis <dennis-farris@hotmail.com>
2025-09-11 14:44:51 -0400

    set up home to show data from fetching

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,23 @@
-import products from '../data/products';
-import ProductCard from '../components/ProductCard';
+import ProductCard from "../components/ProductCard";
+import useFetch from "../../hooks/useFetch";
 
 export default function Home() {
+  const { loading, data, error } = useFetch("/products");
+
   return (
     <div className="p-6">
-      <h2 className="text-3xl font-anime mb-6 text-barbiePink">Welcome to Anime Barbie Shop!</h2>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-        {products.map((product) => (
-          <ProductCard key={product.id} product={product} />
-        ))}
-      </div>
+      <h2 className="text-3xl font-anime mb-6 text-barbiePink">
+        Welcome to Anime Barbie Shop!
+      </h2>
+      {error && <p>Internal server error</p>}
+      {loading && <p>data loading-replace with spinner wheel later</p>}
+      {data && (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+          {data.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
